c451d3d3ecd134bc14264a94db0ada04d0139d02
s20183060 <s20183060@hcs70237.stdlan.hcs.local>
2019-06-26 11:16:15 +0900

    削除変更

--- src/app/DeleteServlet.java
+++ src/app/DeleteServlet.java
@@ -1,6 +1,9 @@
 package app;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,18 +19,26 @@ import javax.servlet.http.HttpSession;
 public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	String drvName = "oracle.jdbc.driver.OracleDriver";// OracleのJDBCﾄﾞﾗｲﾊﾞ名
+	String url = "jdbc:oracle:thin:@"; // プロトコル名称，サブプロトコル名称，及びサブネーム
+	String servername = "10.11.39.215"; // DBホスト名称
+	String port = "1521"; // サーバが接続を監視する(リスナー)ポート番号
+	String sid = "HCS1"; // インスタンス識別子
+	String user = "h20183060"; // オラクル接続ユーザー（各自の設定値）
+	String pass = "oraclemaster"; // オラクル接続ユーザーのパスワード
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		int index = Integer.parseInt(request.getParameter("index"));
 
 		HttpSession session = request.getSession();
-		S2A205infoBean InfoBean = (S2A205infoBean) session.getAttribute("InfoBean");
-		if (InfoBean == null) {
-			InfoBean = new S2A205infoBean();
-		}
+		//S2A205infoBean InfoBean = (S2A205infoBean) session.getAttribute("InfoBean");
+		//if (InfoBean == null) {
+		//	InfoBean = new S2A205infoBean();
+		//}
 
-		if (index >= InfoBean.getArraySize()) {
+		if (index >= infoBean.getArraySize()) {
 			getServletContext().getRequestDispatcher("/errorinput.html")
 					.forward(request, response);
 			return;
@@ -39,22 +50,31 @@ public class DeleteServlet extends HttpServlet {
 		String text;
 		String memo;
 		String li;
-
+		S2A205infoBean infoBean = new S2A205infoBean();
 		important = request.getParameter("important");
 		text = request.getParameter("text");
 		memo = request.getParameter("memo");
 		li = request.getParameter("li");
 
-		S2A205RecordBean stdRecord = new S2A205RecordBean();
-		stdRecord.setImportant(important);
-		stdRecord.setText(text);
-		stdRecord.setMemo(memo);
-		stdRecord.setLi(li);
-		InfoBean.deletestudentRecord(index - 1);
+		try {
+			Class.forName(drvName);
+			try (Connection conn = DriverManager.getConnection(url + servername + ":" + port + ":" + sid, user, pass);
+					Statement stmt = conn.createStatement();) {
+				stmt.executeUpdate("DELETE FROM STUDENT WHERE  IMPORTANT=index");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		session.setAttribute("InfoBean", InfoBean);
+		//S2A205RecordBean stdRecord = new S2A205RecordBean();
+		//stdRecord.setImportant(important);
+		//stdRecord.setText(text);
+		//stdRecord.setMemo(memo);
+		//stdRecord.setLi(li);
+		//InfoBean.deletestudentRecord(index - 1);
+
+		session.setAttribute("InfoBean", infoBean);
 		getServletContext()
 				.getRequestDispatcher("/output2.jsp").forward(request, response);
 	}
-
 }
